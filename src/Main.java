db1778d4cf8934274d789027fba77a14b3dc66e9
nylecm <michalnylec@gmail.com>
2020-10-29 10:50:11 +0000

    Main class added.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * The type Main.
+ */
+public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
