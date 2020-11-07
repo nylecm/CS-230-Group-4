b0faa64af6365a75c5138b9418ceff6b2f653d44
nylecm <michalnylec@gmail.com>
2020-11-07 22:15:17 +0000

    Login Page Background Image

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
 /**
  * The type Main.
  */
-public class Main {
+public class Main extends Application {
+    public void start(Stage primaryStage) {
+        try {
+            Pane root = (Pane) FXMLLoader.load(getClass().getResource("log_in.fxml"));
+            Scene scene = new Scene(root,400,200);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * The entry point of application.
      *
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
