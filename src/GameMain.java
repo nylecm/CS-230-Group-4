aed10ab9583cdef99d55c6e508da6932bb7e4415
nylecm <michalnylec@gmail.com>
2020-11-10 23:49:59 +0000

    game.fxml & related classes complete. Minor change to LogInMain.java

---
+++ src/GameMain.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.layout.Pane;
+        import javafx.stage.Stage;
+
+/**
+ * The type Main.
+ */
+public class GameMain extends Application {
+    public void start(Stage primaryStage) {
+        try {
+            Pane root = (Pane) FXMLLoader.load(getClass().getResource("game.fxml"));
+            Scene scene = new Scene(root,720,576);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
