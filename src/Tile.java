1080f82ca7e2403f91a161695dae7b9e4ff21234
MNabina <monabina1@gmail.com>
2020-11-11 22:27:33 +0300

    initial tile hierarchy created

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,27 @@
+/**
+ * The type Tile.
+ * @author mnabina
+ */
 public abstract class Tile {
+
+    private final String typeName;
+
+    /**
+     * Instantiates a new Tile.
+     *
+     * @param typeName the type name
+     */
+    public Tile(String typeName) {
+        this.typeName = typeName;
+    }
+
+    /**
+     * Gets type name.
+     *
+     * @return the type name
+     */
+    public String getTypeName() {
+        return typeName;
+    }
 }
+
