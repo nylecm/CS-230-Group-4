51b9da9fc0044da71e149ba629c154b21360f4f3
MNabina <monabina1@gmail.com>
2020-11-11 22:27:33 +0300

    initial tile hierarchy created

--- src/ActionTile.java
+++ src/ActionTile.java
@@ -1,2 +1,21 @@
+/**
+ * The type Action tile.
+ * @author mnabina
+ */
 public class ActionTile extends Tile {
+    /**
+     * Instantiates a new Action tile.
+     *
+     * @param typeName the type name
+     */
+    public ActionTile(String typeName) {
+        super(typeName);
+    }
+
+    /**
+     * Use.
+     */
+    public void use() {
+
+    }
 }
--- src/FloorTile.java
+++ src/FloorTile.java
@@ -1,2 +1,51 @@
+/**
+ * The type Floor tile.
+ * @author mnabina
+ */
 public class FloorTile extends Tile {
+    private final boolean isFixed;
+    private final int paths;
+    private final boolean isGoalTile;
+
+    /**
+     * Instantiates a new Floor tile.
+     *
+     * @param typeName   the type name
+     * @param isFixed    the is fixed
+     * @param paths      the paths
+     * @param isGoalTile the is goal tile
+     */
+    public FloorTile(String typeName, boolean isFixed, int paths, boolean isGoalTile) {
+        super(typeName);
+        this.isFixed = isFixed;
+        this.paths = paths;
+        this.isGoalTile = isGoalTile;
+    }
+
+    /**
+     * Is fixed boolean.
+     *
+     * @return the boolean
+     */
+    public boolean isFixed() {
+        return isFixed;
+    }
+
+    /**
+     * Gets paths.
+     *
+     * @return the paths
+     */
+    public int getPaths() {
+        return paths;
+    }
+
+    /**
+     * Is goal tile boolean.
+     *
+     * @return the boolean
+     */
+    public boolean isGoalTile() {
+        return isGoalTile;
+    }
 }
