356de4d8de74176f12d8f5e134461bba2b95d6af
nylecm <michalnylec@gmail.com>
2020-11-11 20:57:11 +0000

    Main classes tweaked so GameMain.java opens the game window for testing, and Main.java opens the main menu, which has not yet been created.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Scene;
-        import javafx.scene.layout.Pane;
-        import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * The type Main.
  */
-public class GameMain extends Application {
+public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
-            Pane root = (Pane) FXMLLoader.load(getClass().getResource("game.fxml"));
+            Pane root = (Pane) FXMLLoader.load(getClass().getResource("main.fxml"));
             Scene scene = new Scene(root, 720, 576);
 
             primaryStage.setScene(scene);
