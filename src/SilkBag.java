a38fa89a3cc4298e2605eba8d306533086630306
nylecm <michalnylec@gmail.com>
2020-11-12 10:57:21 +0000

    Silk bag empty methods created.

--- src/SilkBag.java
+++ src/SilkBag.java
@@ -1,2 +1,19 @@
 public class SilkBag {
+    private Tile[] tiles;
+
+    public SilkBag(int cap) {
+        tiles = new Tile[cap];
+    }
+
+    public void add(Tile t) {
+        System.out.println("You've been had to believe this works!");
+    }
+
+    public Tile take() {
+        return null;
+    }
+
+    public Tile takeFloor() {
+        return null;
+    }
 }
