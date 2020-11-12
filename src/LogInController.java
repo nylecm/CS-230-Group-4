18d23f2e9f73873c83c92233d7fe584c4870bd04
Girion-dev <mat.hladky@gmail.com>
2020-11-12 21:27:12 +0000

    Login page edited

--- src/LogInController.java
+++ src/LogInController.java
@@ -1,24 +1,27 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-import javax.swing.*;
-
 public class LogInController {
-    @FXML private Button button;
-    @FXML private TextField username;
-    @FXML private PasswordField password;
-    @FXML private Button logInButton;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button logInButton;
 
     @FXML
     private void handleCalculateButtonAction(ActionEvent event) {
         String x = username.getText();
         String y = password.getText();
 
-        //fixme remove this:
-        JOptionPane.showMessageDialog(null, "Hello");
     }
 }
