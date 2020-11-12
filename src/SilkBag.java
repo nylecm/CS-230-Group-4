e170fae5e73a05d835b3c6f51ab6771e855aeafb
nylecm <michalnylec@gmail.com>
2020-11-12 13:59:19 +0000

    SilkBag.java add method renamed to put. Java Doc Created.

--- src/SilkBag.java
+++ src/SilkBag.java
@@ -1,30 +1,31 @@
 import java.util.Arrays;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * The type Silk bag.
+ * A silk bag holds tiles of arbitrary type. Tiles can be put into the silk bag,
+ * a random tile can be taken out of a silk bag, and a random floor tile can be
+ * taken out of the silk bag.
+ *
+ * @author nylecm, paired with ashrw0
  */
 public class SilkBag {
-    //private Set<Integer> curPosOccupied;
-    private Tile[] tiles;
+    private final Tile[] tiles;
 
     /**
-     * Instantiates a new Silk bag.
+     * Instantiates a new Silk bag, of a set capacity.
      *
      * @param cap The capacity of the silk bag.
      */
     public SilkBag(int cap) {
         tiles = new Tile[cap];
-        //curPosOccupied = new int[];
     }
 
     /**
-     * Add.
+     * Puts a tile into the silk bag.
      *
-     * @param t the tile
+     * @param t the tile to be put into the silk bag.
      */
-    public void add(Tile t) {
+    public void put(Tile t) {
         boolean isTileAdded = false;
         int i = 0;
 
@@ -38,13 +39,13 @@ public class SilkBag {
     }
 
     /**
-     * Take tile.
+     * Takes a random tile out of the silk bag.
      *
-     * @return the tile
-     * @throws IllegalStateException the illegal state exception
+     * @return a random tile from the silk bag.
+     * @throws IllegalStateException when no tiles are in the silk bag.
      */
     public Tile take() throws IllegalStateException {
-        if (isEmpty()) {
+        if (isTileLess()) {
             throw new IllegalStateException("No tiles in silk bag!");
         }
 
@@ -60,11 +61,12 @@ public class SilkBag {
     }
 
     /**
-     * Take floor floor tile.
+     * Takes a random floor tile out of the silk bag.
      *
-     * @return the floor tile
+     * @return a random floor tile from the silk bag.
+     * @throws IllegalStateException when no floor tiles are in the silk bag.
      */
-    public FloorTile takeFloor() {
+    public FloorTile takeFloor() throws IllegalStateException {
         if (isFloorTileLess()) {
             throw new IllegalStateException("No floor tiles in silk bag!");
         }
@@ -80,18 +82,28 @@ public class SilkBag {
         }
     }
 
-    private boolean isFloorTileLess() {
-        for (Tile t: tiles) {
-            if (t instanceof FloorTile) {
+    /**
+     * Checks if there are any tiles in the silk bag.
+     *
+     * @return true if no tiles are found.
+     */
+    private boolean isTileLess() {
+        for (Tile t : tiles) {
+            if (t != null) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean isEmpty() {
-        for (Tile t: tiles) {
-            if (t != null) {
+    /**
+     * Checks if there are any floor tiles in the silk bag.
+     *
+     * @return true if no floor tiles are found.
+     */
+    private boolean isFloorTileLess() {
+        for (Tile t : tiles) {
+            if (t instanceof FloorTile) {
                 return false;
             }
         }
@@ -106,42 +118,44 @@ public class SilkBag {
     }
 
     /**
-     * The entry point of application.
+     * The entry point of application, for testing only. todo remove this.
      *
      * @param args the input arguments
      */
     public static void main(String[] args) {
         SilkBag s = new SilkBag(1000);
 
-        s.add(new ActionTile("fire"));
-        s.add(new ActionTile("ice"));
-        s.add(new ActionTile("back_track"));
-        s.add(new ActionTile("double_move"));
-        s.add(new FloorTile("cross", false, 100, false));
-        s.add(new ActionTile("Fire"));
-        s.add(new ActionTile("Fire"));
-        s.add(new ActionTile("Fire"));
-        s.add(new ActionTile("Fire"));
-        s.add(new ActionTile("Fire"));
+        s.put(new ActionTile("fire"));
+        s.put(new ActionTile("ice"));
+        s.put(new ActionTile("back_track"));
+        s.put(new ActionTile("double_move"));
+        s.put(new FloorTile("cross", false, 1111, false));
+        s.put(new ActionTile("Fire"));
+        s.put(new ActionTile("Fire"));
+        s.put(new ActionTile("Fire"));
+        s.put(new ActionTile("Fire"));
+        s.put(new ActionTile("Fire"));
 
         System.out.println(Arrays.toString(s.tiles));
 
         Tile t1 = s.take();
 
-
         System.out.println(Arrays.toString(s.tiles));
 
-        s.take();
-        s.take();
-        s.take();
-        s.take();
-        s.take();
 
         System.out.println(s.takeFloor());
 
+        s.take();
+        s.take();
+        s.take();
+        s.take();
+        s.take();
+
+
         System.out.println(Arrays.toString(s.tiles));
 
 
+        /*s.take();
         s.take();
         s.take();
         s.take();
@@ -150,8 +164,7 @@ public class SilkBag {
         s.take();
         s.take();
         s.take();
-        s.take();
-        s.take();
+        s.take();*/
 
         System.out.println(Arrays.toString(s.tiles));
     }
