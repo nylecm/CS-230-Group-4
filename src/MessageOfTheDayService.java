9b82ce4699c889cf9ff32ada2d639d38aaa01107
nylecm <michalnylec@gmail.com>
2020-11-12 21:02:30 +0000

    MessageOfTheDayService.java JavaDocs updated, minor tweaks to code structure, and exception type changed to IOException, and is no longer handled by the getMessage() method.

--- src/MessageOfTheDayService.java
+++ src/MessageOfTheDayService.java
@@ -1,65 +1,54 @@
+import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
 /**
  * This class represents the message of the day for the game that will be displayed on startup
  *
- * @author Samuel Cox
- * @version 1.1
+ * @author SamCox7500, minor improvements by nylecm
  */
 public class MessageOfTheDayService {
-
-
     /**
-     * Returns the message of the day
+     * Gets the message of the day.
      *
-     * @return String containing message of the day. If motd is unable to be retrieved then an empty string.
-     * @throws Exception
+     * @return String containing message of the day.
+     * @throws IOException when the URL is malformed.
      */
-    public static String getMessage() throws Exception {
-        try {
+    public static String getMessage() throws IOException {
+        Scanner puzzleIn = readURL("http://cswebcat.swansea.ac.uk/puzzle");
+        String puzzle = puzzleIn.nextLine();
+        String shiftedPuzzle = shiftPuzzle(puzzle);
 
-            Scanner in = readURL("http://cswebcat.swansea.ac.uk/puzzle");
-            String puzzle = in.nextLine();
-            String shiftedPuzzle = shiftPuzzle(puzzle);
-            String courseCode = "CS-230";
-            String codeAndShiftedPuzzle = courseCode + shiftedPuzzle;
-            String puzzleSolution = codeAndShiftedPuzzle + codeAndShiftedPuzzle.length();
+        String courseCode = "CS-230";
+        String codeAndShiftedPuzzle = courseCode + shiftedPuzzle;
 
-            Scanner solIn = readURL("http://cswebcat.swansea.ac.uk/message?solution=" + puzzleSolution);
-            String message = solIn.nextLine();
-            return message;
+        String puzzleSolution = codeAndShiftedPuzzle + codeAndShiftedPuzzle.length();
 
-        } catch (Exception e) {
-            return "";
-        }
+        Scanner messageIn = readURL("http://cswebcat.swansea.ac.uk/message?solution=" + puzzleSolution);
+        return messageIn.nextLine();
     }
 
     /**
      * Reads the contents of the web page specified by url.
      *
      * @param urlName The url for the webpage that is to be read from
-     * @return A scanner containing the contents of the web page
-     * @throws Exception
+     * @return A scanner reading the contents of the web page
+     * @throws IOException when the URL is malformed.
      */
-    private static Scanner readURL(String urlName) throws Exception {
-
+    private static Scanner readURL(String urlName) throws IOException {
         URL url = new URL(urlName);
-        Scanner in = new Scanner(url.openStream());
-
-        return in;
+        return new Scanner(url.openStream());
     }
 
     /**
-     * Applies incrementing caesar shift to each character in revolving directions.
+     * Applies a caesar shift to a message, in alternating directions. Eg. one
      * I.e. first letter shifted 1 backwards, second letter shifted 2 forwards,
      * third letter shifted 3 backwards...
      *
      * @param puzzle The string that the caesar shift is to be applied to
-     * @return The string in which each character has been shifted
+     * @return The decrypted string
      */
     private static String shiftPuzzle(String puzzle) {
-
         final int A_ASCII_VALUE = 65;
         final int Z_ASCII_VALUE = 90;
         final int ALPHABET_SIZE = 26;
@@ -68,25 +57,28 @@ public class MessageOfTheDayService {
         for (int i = 0; i < puzzleLetters.length; i += 2) {
             puzzleLetters[i] = (char) (puzzleLetters[i] - (i + 1));
             if (puzzleLetters[i] < A_ASCII_VALUE) {
-                puzzleLetters[i] = (char) (puzzleLetters[i] + 26);
+                puzzleLetters[i] = (char) (puzzleLetters[i] + ALPHABET_SIZE);
             }
         }
         for (int i = 1; i < puzzleLetters.length; i += 2) {
             puzzleLetters[i] = (char) (puzzleLetters[i] + (i + 1));
             if (puzzleLetters[i] > Z_ASCII_VALUE) {
-                puzzleLetters[i] = (char) (puzzleLetters[i] - 26);
+                puzzleLetters[i] = (char) (puzzleLetters[i] - ALPHABET_SIZE);
             }
         }
-
-        //System.out.println(String.valueOf(puzzleLetters));
         return String.valueOf(puzzleLetters);
-
     }
 
-
-    public static void main(String[] args) throws Exception {
-
-        System.out.println(getMessage());
+    /**
+     * Main method, used for testing only.
+     *
+     * @param args the command-line arguments.
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
