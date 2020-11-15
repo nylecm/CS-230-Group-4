6d7c2185aae7442aad25fafec2c6abf36d40f071
Girion-dev <mat.hladky@gmail.com>
2020-11-15 13:53:43 +0000

    Fix push

--- src/LogInController.java
+++ src/LogInController.java
@@ -1,9 +1,16 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import security.LoginHandler;
+
+import java.io.IOException;
 
 public class LogInController {
 
@@ -21,5 +28,19 @@ public class LogInController {
         String username = this.username.getText();
         String password = this.password.getText();
         System.out.println(LoginHandler.login(username, password));
+    }
+
+    @FXML
+    private void onRegisterButtonClicked(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane register = (Pane) FXMLLoader.load(getClass().getResource("register.fxml"));
+        currentStage.setScene(new Scene(register));
+    }
+
+    @FXML
+    private void onRegisterButtonScrolled(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane register = (Pane) FXMLLoader.load(getClass().getResource("register.fxml"));
+        currentStage.setScene(new Scene(register));
     }
 }
