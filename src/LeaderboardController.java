58d7dcf0e166915766033af339fd37e3869cc9d7
nylecm <michalnylec@gmail.com>
2020-11-16 12:30:33 +0000

    LeaderboardController.java back button returns to main menu.

--- src/LeaderboardController.java
+++ src/LeaderboardController.java
@@ -1,3 +1,18 @@
-public class LeaderboardController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class LeaderboardController {
+    @FXML
+    private void onBackButtonClicked(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane mainMenu = (Pane) FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        currentStage.setScene(new Scene(mainMenu));
+    }
 }
