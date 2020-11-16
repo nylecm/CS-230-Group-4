2e398c96f5d4a35369ecf901f563c8e25a6c2230
ashrw0 <waleedashraf1@outlook.com>
2020-11-16 17:04:56 +0000

    Initial development of Gameboard class

--- src/java_/game/tile/GameBoard.java
+++ src/java_/game/tile/GameBoard.java
@@ -1,4 +1,22 @@
 package java_.game.tile;
 
+import java_.game.player.PlayerPiece;
+
 public class GameBoard {
+
+    private int width;
+    private int height;
+    private String name;
+    private PlayerPiece[] playerPiece = new PlayerPiece[4];
+    private int[][] coordinate = new int[2][4];
+    private SilkBag silkBag;
+    private FloorTile[][] board = new FloorTile[width][height];
+
+
+    public GameBoard(PlayerPiece[] playerPiece, int width, int height, String name, SilkBag silkBag) {
+        this.playerPiece = playerPiece;
+        this.name = name;
+        this.silkBag = silkBag;
+    }
+
 }
