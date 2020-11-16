5049eaf5518b11365047ad5eb22da1f69d4bf325
nylecm <michalnylec@gmail.com>
2020-11-16 13:46:49 +0000

    Game Window navigation Quit & Save & Quit (save not functional).

--- src/GameController.java
+++ src/GameController.java
@@ -1,5 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GameController {
-    public static void main(String[] args) {
-        System.out.println();
+    @FXML
+    private void onSaveButtonClicked(ActionEvent e) {
+        //todo Save Game
+    }
+
+    @FXML
+    private void onSaveExitButtonClicked(ActionEvent e) throws IOException {
+        //todo Save Game
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane mainMenu = (Pane) FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        currentStage.setScene(new Scene(mainMenu));
+    }
+
+    @FXML
+    private void onExitButtonClicked(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane mainMenu = (Pane) FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        currentStage.setScene(new Scene(mainMenu));
     }
 }
