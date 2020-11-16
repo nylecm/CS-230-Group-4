1b519dba396822025f9f9a2cf0b214741b53e444
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-16 18:18:23 +0000

    Partially completed insertAt method in GameBoard.java.

--- src/java_/game/tile/GameBoard.java
+++ src/java_/game/tile/GameBoard.java
@@ -23,4 +23,60 @@ public class GameBoard {
     }
 
 
+    private boolean isRowFixed(int posY) {
+
+        for (int x = 0; x < width; x++) {
+
+            if (board[x][posY].isFixed()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isColumnFixed(int posX) {
+
+        for (int y = 0; y < height; y++) {
+
+            if (board[posX][y].isFixed()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void insertAt(int positionX, int positionY, FloorTile tile) throws IndexOutOfBoundsException{
+        if (tile.isFixed() && board[positionX][positionY] == null) {
+            board[positionX][positionY] = tile;
+        } else {
+
+            if ((positionX == 0 || positionX == width) || positionY == 0 || positionY == height) {
+
+               if (!isColumnFixed(positionX) && !isRowFixed(positionY)) {
+
+
+
+                   board[positionX][positionY] = tile;
+
+
+
+
+               }
+
+
+
+
+                board[positionX][positionY] = tile;
+
+
+
+
+
+
+            } else {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+    }
+
+
 }
