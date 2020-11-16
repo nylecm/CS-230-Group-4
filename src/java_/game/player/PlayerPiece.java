dff1b9e4a5521c9bf113e79e24e0f52ac2eca1c0
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-16 18:42:15 +0000

    Created initial PlayerPiece.java class.

--- src/java_/game/player/PlayerPiece.java
+++ src/java_/game/player/PlayerPiece.java
@@ -1,4 +1,21 @@
 package java_.game.player;
 
+import java_.util.Position;
+
 public class PlayerPiece {
+
+    private Position position;
+
+    public PlayerPiece(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
 }
