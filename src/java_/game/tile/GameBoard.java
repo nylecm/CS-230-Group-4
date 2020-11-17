dd453defa7c9ec10787db1b51430a4b0b50e9347
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-17 16:10:27 +0000

    Created slideTiles method in GameBoard.java.

--- src/java_/game/tile/GameBoard.java
+++ src/java_/game/tile/GameBoard.java
@@ -1,13 +1,17 @@
 package java_.game.tile;
 
 import java_.game.player.PlayerPiece;
+import java_.util.Position;
+
+import java.util.HashMap;
 
 public class GameBoard {
 
-    private int width;
-    private int height;
+    private final int width;
+    private final int height;
     private String name;
     private PlayerPiece[] playerPiece;
+    //private HashMap<PlayerPiece, Position> playerPositionMap;
     private int[][] coordinate = new int[2][4];
     private SilkBag silkBag;
     private FloorTile[][] board;
@@ -44,15 +48,39 @@ public class GameBoard {
         return false;
     }
 
+    private void slideTiles(int posX, int posY) {
+
+        FloorTile temp1;
+        FloorTile temp2;
+
+        if (posX == -1) {
+
+            temp1 = board[0][posY];
+            temp2 = board[1][posY];
+            board[1][posY] = temp1;
+            board[0][posY] = null;
+
+            for (int i = 2; i < width; i +=2) {
+                temp1 = board[i][posY];
+                board[i][posY] = temp2;
+                temp2 = board[i+1][posY];
+                board[i+1][posY] = temp1;
+            }
+        }
+
+
+
+    }
+
+
     public void insertAt(int positionX, int positionY, FloorTile tile) throws IndexOutOfBoundsException{
         if (tile.isFixed() && board[positionX][positionY] == null) {
             board[positionX][positionY] = tile;
         } else {
 
-            if ((positionX == 0 || positionX == width) || positionY == 0 || positionY == height) {
+            if (positionX == -1 || positionX == width +1 || positionY == -1 || positionY == height + 1) {
 
-               if (!isColumnFixed(positionX) && !isRowFixed(positionY)) {
-
+               if (!isColumnFixed(positionX + 1) && !isRowFixed(positionY + 1)) {
 
 
                    board[positionX][positionY] = tile;
