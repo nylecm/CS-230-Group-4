8b0493abef1300af73a2f74bea7e36e76db94ed8
nylecm <michalnylec@gmail.com>
2020-11-19 01:06:28 +0000

    Started to clean-up game board reader a little bit.

--- src/java_/game/controller/GameService.java
+++ src/java_/game/controller/GameService.java
@@ -1,16 +1,12 @@
 package java_.game.controller;
 
-import java_.game.player.Player;
 import java_.game.player.PlayerPiece;
 import java_.game.player.PlayerService;
 import java_.game.tile.*;
 import java_.util.Position;
-import javafx.geometry.Pos;
-import sun.security.util.ArrayUtil;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,6 +18,8 @@ public class GameService {
     private GameBoard gb;
     private PlayerService ps;
     private int turnCount;
+
+    private static final String DELIMITER = "` ";
 
     private GameService() {
         //ps = PlayerService.getInstance().remake();
@@ -39,19 +37,20 @@ public class GameService {
         remake();
 
         Scanner in = new Scanner(f);
-        in.useDelimiter("` ");
+        in.useDelimiter(DELIMITER);
 
-        int nRows;
-        int nCols;
-        PlayerPiece[] playerPieces = new PlayerPiece[nPlayers];
+        readSelectGameBoard(boardName, nPlayers, in);
+        in.close();
+    }
 
-        while (in.hasNextLine() && in.next().equals(boardName)) {
-            //todo to be completed fully when other classes complete...
+    private void readSelectGameBoard(String boardName, int nPlayers, Scanner in) {
+        while (in.hasNextLine() && in.next().equals(boardName)) {//todo to be completed fully when other classes complete...
+            System.out.println(boardName); //todo remove
 
-            System.out.println(boardName);
+            int nRows = in.nextInt();
+            int nCols = in.nextInt();
 
-            nRows = in.nextInt();
-            nCols = in.nextInt();
+            // Dealing with fixed tiles:
 
             int nFixedTiles = in.nextInt();
             FloorTile[] fixedTiles = new FloorTile[nFixedTiles];
@@ -69,6 +68,8 @@ public class GameService {
             }
             System.out.println(Arrays.toString(fixedTiles)); //todo remove
 
+            // Dealing with non-fixed floor tiles:
+
             ArrayList<FloorTile> floorTiles = new ArrayList<>();
 
             for (int i = 0; i < FloorTile.FLOOR_TILE_TYPES.size(); i++) {
@@ -80,11 +81,13 @@ public class GameService {
                     floorTiles.add(t); //todo replace with gb.insert at...
                 }
             }
-
             System.out.println(floorTiles);
+
             Collections.shuffle(floorTiles);
+
             System.out.println(floorTiles);
 
+            // Taking first floor tiles for the initial set to populate game board.
             FloorTile[] floorTilesForGameBoard = new FloorTile[(nRows * nCols) - nFixedTiles]; // todo check if nFixed Tiles is greater than nCol * nRow
 
             for (int i = 0; i < (nRows * nCols) - nFixedTiles; i++) { //todo check if there are enough tiles for the game board...
@@ -93,30 +96,21 @@ public class GameService {
             }
 
             //Action tiles:
-            ArrayList<ActionTile> actionTiles = new ArrayList<>();
-
-            for (int i = 0; i < ActionTile.ACTION_TILE_TYPES.size(); i++) {
-                TileType tileType = TileType.valueOf(in.next().toUpperCase());
-                int n = in.nextInt();
-                ActionTile t = new ActionTile(tileType);
-
-                for (int j = 0; j < n; j++) {
-                    actionTiles.add(t);
-                }
-            }
+            ArrayList<ActionTile> actionTiles = readActionTiles(in);
             System.out.println(actionTiles);
 
             Collections.shuffle(actionTiles);
 
+            // Silk Bag:
             ArrayList<Tile> tilesForSilkBag = new ArrayList<>();
             tilesForSilkBag.addAll(floorTiles);
             tilesForSilkBag.addAll(actionTiles);
 
-            //silk bag...
             SilkBag sb = new SilkBag(tilesForSilkBag.toArray(new Tile[0]));
             System.out.println(sb);
 
-            // Player Pieces
+            // Player Pieces:
+            PlayerPiece[] playerPieces = new PlayerPiece[nPlayers];
             for (int i = 0; i < nPlayers; i++) {
                 int startRow = in.nextInt();
                 int startCol = in.nextInt();
@@ -124,15 +118,25 @@ public class GameService {
             }
 
             gb = new GameBoard(playerPieces, fixedTiles, fixedTilePositions, floorTilesForGameBoard, nCols, nRows, boardName, sb);
-            System.out.println(gb);
+            System.out.println(gb); // todo consider keeping silk bag in game service...
         }
-        in.close();
-
-        // Player Serv create...
-        // Silk bag create ...
-        // Game board create ...
-        // UI Data Bundle...
     }
+
+    private ArrayList<ActionTile> readActionTiles(Scanner in) {
+        ArrayList<ActionTile> actionTiles = new ArrayList<>();
+
+        for (int i = 0; i < ActionTile.ACTION_TILE_TYPES.size(); i++) {
+            TileType tileType = TileType.valueOf(in.next().toUpperCase());
+            int n = in.nextInt();
+            ActionTile t = new ActionTile(tileType);
+
+            for (int j = 0; j < n; j++) {
+                actionTiles.add(t);
+            }
+        }
+        return actionTiles;
+    }
+
 
     public void loadSavedInstance(File f) throws FileNotFoundException {
         remake(); //todo future homer's problem
