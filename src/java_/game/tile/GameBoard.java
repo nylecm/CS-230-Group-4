5736f99f303d1a89d1e8fd027b399c7f8e21f20b
nylecm <michalnylec@gmail.com>
2020-11-20 15:26:24 +0000

    GameBoard.java formatting improved.

--- src/java_/game/tile/GameBoard.java
+++ src/java_/game/tile/GameBoard.java
@@ -35,109 +35,72 @@ public class GameBoard {
     }
 
     private boolean isRowFixed(int rowNum) {
-
         if (rowNum == -1) {
             rowNum += 1;
         } else if (rowNum == nRows) {
             rowNum -= 1;
         }
         for (int x = 0; x < nCols; x++) {
-
             if (board[rowNum][x].isFixed()) { //Check for frozen etc.
                 return true;
             }
         }
-        return false;
-
-        /**
-        if (rowNum == -1) {
-            for (int x = 0; x < nCols; x++) {
-
-                if (board[rowNum+1][x].isFixed()) { //Check for frozen etc.
-                    return true;
-                }
-            }
-            return false;
-        } else if (rowNum == nRows) {
-            for (int x = 0; x < nCols; x++) {
-
-                if (board[rowNum-1][x].isFixed()) {
-                    return true;
-                }
-            }
-            return false;
-        } else {
-            for (int x = 0; x < nCols; x++) {
-
-                if (board[rowNum][x].isFixed()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-
-    **/
+        return false; // See bottom for commented out code...
     }
 
     private boolean isColumnFixed(int colNum) {
-
-
         if (colNum == -1) {
             colNum += 1;
         } else if (colNum == nCols) {
             colNum -= 1;
         }
         for (int y = 0; y < nRows; y++) {
-
             if (board[y][colNum].isFixed()) {
                 return true;
             }
         }
         return false;
         /**
-        if (colNum == -1) {
-            for (int y = 0; y < nRows; y++) {
+         if (colNum == -1) {
+         for (int y = 0; y < nRows; y++) {
 
-                if (board[y][colNum+1].isFixed()) {
-                    return true;
-                }
-            }
-            return false;
-        } else if (colNum == nCols) {
-            for (int y = 0; y < nRows; y++) {
+         if (board[y][colNum+1].isFixed()) {
+         return true;
+         }
+         }
+         return false;
+         } else if (colNum == nCols) {
+         for (int y = 0; y < nRows; y++) {
 
-                if (board[y][colNum-1].isFixed()) {
-                    return true;
-                }
-            }
-            return false;
-        } else {
-            for (int y = 0; y < nRows; y++) {
+         if (board[y][colNum-1].isFixed()) {
+         return true;
+         }
+         }
+         return false;
+         } else {
+         for (int y = 0; y < nRows; y++) {
 
-                if (board[y][colNum].isFixed()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        **/
+         if (board[y][colNum].isFixed()) {
+         return true;
+         }
+         }
+         return false;
+         }
+         **/
     }
 
     private void insertFixedTiles(FloorTile[] fixedTiles, Position[] fixedTilePositions) {
-
-        for (int i = 0; i < fixedTiles.length; i++ ) {
+        for (int i = 0; i < fixedTiles.length; i++) {
             int rowNum = fixedTilePositions[i].getrowNum();
             int colNum = fixedTilePositions[i].getcolNum();
 
             if (board[rowNum][colNum] == null) {
                 board[rowNum][colNum] = fixedTiles[i];
             }
-
         }
     }
-    private void fillGaps(FloorTile[] tiles) {
 
+    private void fillGaps(FloorTile[] tiles) {
         int nextFloorTile = 0;
 
         for (int i = 0; i < nRows; i++) {
@@ -145,53 +108,52 @@ public class GameBoard {
 
                 if (board[i][j] == null) {
                     board[i][j] = tiles[nextFloorTile];
-                    nextFloorTile +=1;
+                    nextFloorTile += 1;
                 }
             }
         }
     }
 
 
-                //insertAt
-    private void insert(int colNum /**x**/, int rowNum /**y**/, FloorTile tile) {
-
+    //insertAt
+    private void insert(int colNum, int rowNum, FloorTile tile) {
         FloorTile pushedOffTile = null; //Being pushed off
 
         if (colNum == -1) {
-            pushedOffTile = board[rowNum][nCols-1];
+            pushedOffTile = board[rowNum][nCols - 1];
 
             for (int i = nCols - 1; i != 0; i--) {
 
-                board[rowNum][i] = board[rowNum][i-1];
-                board[rowNum][i-1] = null;
+                board[rowNum][i] = board[rowNum][i - 1];
+                board[rowNum][i - 1] = null;
 
             }
 
-            board[rowNum][colNum+1] = tile;
+            board[rowNum][colNum + 1] = tile;
         } else if (colNum == nCols) {
             pushedOffTile = board[rowNum][0];
 
             for (int i = 0; i < nCols - 1; i++) {
-                board[rowNum][i] = board[rowNum][i+1];
-                board[rowNum][i+1] = null;
+                board[rowNum][i] = board[rowNum][i + 1];
+                board[rowNum][i + 1] = null;
             }
-            board[rowNum][colNum-1] = tile;
+            board[rowNum][colNum - 1] = tile;
         } else if (rowNum == -1) {
-            pushedOffTile = board[nRows-1][colNum];
+            pushedOffTile = board[nRows - 1][colNum];
 
             for (int i = nRows - 1; i != 0; i--) {
-                board[i][colNum] = board[i-1][colNum];
-                board[i-1][colNum] = null;
+                board[i][colNum] = board[i - 1][colNum];
+                board[i - 1][colNum] = null;
             }
-            board[rowNum+1][colNum] = tile;
+            board[rowNum + 1][colNum] = tile;
         } else if (rowNum == nRows) {
             pushedOffTile = board[0][colNum];
 
-            for (int i = 0; i < nRows-1; i++) {
-                board[i][colNum] = board[i+1][colNum];
-                board[i+1][colNum] = null;
+            for (int i = 0; i < nRows - 1; i++) {
+                board[i][colNum] = board[i + 1][colNum];
+                board[i + 1][colNum] = null;
             }
-            board[rowNum-1][colNum] = tile;
+            board[rowNum - 1][colNum] = tile;
         }
 
         silkBag.put(pushedOffTile);
@@ -200,38 +162,38 @@ public class GameBoard {
 
 
     // private  insertAt()
+
     /**
-    public void insertAt(int positionX, int positionY, FloorTile tile) throws IndexOutOfBoundsException {
-        if (tile.isFixed() && board[positionX][positionY] == null) {
-            board[positionX][positionY] = tile;
-        } else {
-
-            if (positionX == -1 || positionX == nCols || positionY == -1 || positionY == nRows) {
-
-                if (!isColumnFixed(positionX) && !isRowFixed(positionY)) {
-
-                    slideTiles(positionX, positionY);
-
-                    if (positionX == -1) {
-                        board[positionY][positionX+1] = tile;
-                    } else if (positionX == nCols) {
-                        board[positionY][positionX-1] = tile;
-                    } else if (positionY == -1) {
-                        board[positionY+1][positionX] = tile;
-                    } else if (positionY == nRows) {
-                        board[positionY-1][positionX] = tile;
-                    }
-
-                }
-
-            } else {
-                throw new IndexOutOfBoundsException();
-            }
-        }
-    }
-    **/
+     * public void insertAt(int positionX, int positionY, FloorTile tile) throws IndexOutOfBoundsException {
+     * if (tile.isFixed() && board[positionX][positionY] == null) {
+     * board[positionX][positionY] = tile;
+     * } else {
+     * <p>
+     * if (positionX == -1 || positionX == nCols || positionY == -1 || positionY == nRows) {
+     * <p>
+     * if (!isColumnFixed(positionX) && !isRowFixed(positionY)) {
+     * <p>
+     * slideTiles(positionX, positionY);
+     * <p>
+     * if (positionX == -1) {
+     * board[positionY][positionX+1] = tile;
+     * } else if (positionX == nCols) {
+     * board[positionY][positionX-1] = tile;
+     * } else if (positionY == -1) {
+     * board[positionY+1][positionX] = tile;
+     * } else if (positionY == nRows) {
+     * board[positionY-1][positionX] = tile;
+     * }
+     * <p>
+     * }
+     * <p>
+     * } else {
+     * throw new IndexOutOfBoundsException();
+     * }
+     * }
+     * }
+     **/
     //to string
-
     public Boolean isWin() {
         return true;
     }
@@ -247,13 +209,11 @@ public class GameBoard {
                 } else {
                     row = row + board[j][i].getType() + " ";
                 }
-
             }
             boardString = boardString + row + "\n";
         }
         return boardString;
     }
-
 
 
     public static void main(String[] args) {
@@ -292,8 +252,7 @@ public class GameBoard {
         tiles[5] = I;
 
 
-
-        GameBoard firstgame = new GameBoard(playerPieces, fixedTiles, fixedTilePositions,tiles, 3, 3, "hello", silkBag);
+        GameBoard firstgame = new GameBoard(playerPieces, fixedTiles, fixedTilePositions, tiles, 3, 3, "hello", silkBag);
 
         System.out.println(firstgame);
 
@@ -301,7 +260,6 @@ public class GameBoard {
         FloorTile insert2 = new FloorTile(TileType.CORNER, false, false);
         FloorTile insert3 = new FloorTile(TileType.T_SHAPED, false, false);
         FloorTile insert4 = new FloorTile(TileType.CORNER, false, false);
-
 
 
         firstgame.insert(-1, 0, insert1);
@@ -314,54 +272,80 @@ public class GameBoard {
         System.out.println(firstgame);
 
 
+        /**
+         firstgame.board[1][0] = D;
+         firstgame.board[1][1] = E;
+         firstgame.board[1][2] = F;
+         firstgame.board[2][0] = G;
+         firstgame.board[2][1] = H;
+         firstgame.board[2][2] = I;
+
+         System.out.println(firstgame);
+         **/
 
 
         /**
-        firstgame.board[1][0] = D;
-        firstgame.board[1][1] = E;
-        firstgame.board[1][2] = F;
-        firstgame.board[2][0] = G;
-        firstgame.board[2][1] = H;
-        firstgame.board[2][2] = I;
+         for (int j = 0; j < firstgame.nRows; j++) {
+         String row = "";
+         for (int i = 0; i < firstgame.nCols; i++) {
+         if (firstgame.board[j][i] == null) {
+         row = row + "Empty ";
+         } else {
+         row = row + firstgame.board[j][i].getType() + " ";
+         }
 
-        System.out.println(firstgame);
-        **/
-
-
-        /**
-        for (int j = 0; j < firstgame.nRows; j++) {
-            String row = "";
-            for (int i = 0; i < firstgame.nCols; i++) {
-                if (firstgame.board[j][i] == null) {
-                    row = row + "Empty ";
-                } else {
-                    row = row + firstgame.board[j][i].getType() + " ";
-                }
-
-            }
-            System.out.println(row);
-        }
-
-        **/
+         }
+         System.out.println(row);
+         }
+         **/
 
         FloorTile insert = new FloorTile(TileType.STRAIGHT, false, false);
 
         //System.out.println("");
         //firstgame.insert(1, 3, insert);
         /**
-        for (int j = 0; j < firstgame.nRows; j++) {
-            String row = "";
-            for (int i = 0; i < firstgame.nCols; i++) {
-                if (firstgame.board[j][i] == null) {
-                    row = row + "Empty ";
-                } else {
-                    row = row + firstgame.board[j][i].getType() + " ";
-                }
+         for (int j = 0; j < firstgame.nRows; j++) {
+         String row = "";
+         for (int i = 0; i < firstgame.nCols; i++) {
+         if (firstgame.board[j][i] == null) {
+         row = row + "Empty ";
+         } else {
+         row = row + firstgame.board[j][i].getType() + " ";
+         }
 
-            }
-            System.out.println(row);
-        }
+         }
+         System.out.println(row);
+         }
          **/
     }
 }
 
+
+/**
+ * // from isRowFixed
+ * if (rowNum == -1) {
+ * for (int x = 0; x < nCols; x++) {
+ * <p>
+ * if (board[rowNum+1][x].isFixed()) { //Check for frozen etc.
+ * return true;
+ * }
+ * }
+ * return false;
+ * } else if (rowNum == nRows) {
+ * for (int x = 0; x < nCols; x++) {
+ * <p>
+ * if (board[rowNum-1][x].isFixed()) {
+ * return true;
+ * }
+ * }
+ * return false;
+ * } else {
+ * for (int x = 0; x < nCols; x++) {
+ * <p>
+ * if (board[rowNum][x].isFixed()) {
+ * return true;
+ * }
+ * }
+ * return false;
+ * }
+ **/
