8c8cd64735b58fd9f6a13ce05b8782c26401effc
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-21 16:56:23 +0000

    Position.java equals and hashCode methods.

--- src/java_/util/Position.java
+++ src/java_/util/Position.java
@@ -1,5 +1,7 @@
 package java_.util;
 
+import java.util.Objects;
+
 public class Position {
 
     private int rowNum;
@@ -28,5 +30,19 @@ public class Position {
     public void setColNum(int colNum) {
 
         this.colNum = colNum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return rowNum == position.rowNum &&
+                colNum == position.colNum;
+    }
+
+    @Override
+    public int hashCode() {
+        return rowNum % 20 + colNum % 20;
     }
 }
