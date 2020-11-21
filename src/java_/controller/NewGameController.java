bb2b3713f1d747be27f0012d2f83d03ef6f0bd68
Girion-dev <mat.hladky@gmail.com>
2020-11-21 16:00:46 +0000

    Added Reader, fixed the biggest bullshit the mankind has ever seen like for realy I'm probably going to kill myself :--)

--- src/java_/controller/NewGameController.java
+++ src/java_/controller/NewGameController.java
@@ -1,16 +1,44 @@
 package java_.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class NewGameController {
+public class NewGameController implements Initializable {
+
+    @FXML
+    private ChoiceBox gameBoardSelect;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Reader reader = new Reader();
+        try {
+            List<String> gameBoardNames = reader.readGameBoardNames();
+            addGameBoardNames(gameBoardNames);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addGameBoardNames(List<String> gameBoardNames) {
+        gameBoardSelect.setItems(FXCollections.observableArrayList(gameBoardNames));
+    }
+
     @FXML
     private void onBackButtonClicked(ActionEvent e) throws IOException {
         Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
---
+++ src/java_/controller/Reader.java
@@ -0,0 +1,40 @@
+package java_.controller;
+
+import java_.game.tile.*;
+import java_.util.Position;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Reader {
+
+    private File file;
+    private Scanner in;
+    private static final String GAME_BOARD_FILE_PATH = "data/game_board.txt";
+    private static final String DELIMITER = "` ";
+
+    public List<String> readGameBoardNames() throws FileNotFoundException {
+        List<String> output = new ArrayList<>();
+        this.file = new File(GAME_BOARD_FILE_PATH);
+        in = new Scanner(file);
+        in.useDelimiter(DELIMITER);
+        int lineCount = 1;
+        while (in.hasNextLine()) {
+            if (lineCount % 2 == 1) {
+                output.add(in.next());
+            }
+            in.nextLine();
+            lineCount++;
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Reader reader = new Reader();
+        reader.readGameBoardNames();
+    }
+}
