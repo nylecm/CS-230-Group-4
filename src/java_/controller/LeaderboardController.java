3dae4ec46ca9643c257ab5bc159e4a39bf3cf9bc
nylecm <michalnylec@gmail.com>
2020-11-29 00:30:02 +0000

    Leaderboard back button.

--- src/java_/controller/LeaderboardController.java
+++ src/java_/controller/LeaderboardController.java
@@ -1,7 +1,7 @@
 package java_.controller;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.Collection;
@@ -16,12 +16,18 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 public class LeaderboardController implements Initializable {
@@ -81,5 +87,12 @@ public class LeaderboardController implements Initializable {
         losses.setCellValueFactory(new PropertyValueFactory<Table, Integer>("rLosses"));
 
         tableID.setItems(data);
+    }
+
+    @FXML
+    private void onBackButtonClicked(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane mainMenu = (Pane) FXMLLoader.load(getClass().getResource("../../view/layout/mainMenu.fxml"));
+        currentStage.setScene(new Scene(mainMenu));
     }
 }
