d5e747b3e79e112ff5855f699b6196bd07d10ed8
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-30 23:08:20 +0000

    JavaDoc for Player.java.

--- src/java_/game/player/Player.java
+++ src/java_/game/player/Player.java
@@ -9,9 +9,12 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * A player is one of up to four participants in the game. They have a PlayerPiece which is
+ * used to represent their position on the board. They are identified by a username.
+ * A player can draw action tiles which are used to apply effects and a player can have effects applied to them.
+ *
  * @author nylecm, coxy7, bishwo
  */
-
 public class Player {
 
     private String username;
@@ -22,7 +25,10 @@ public class Player {
     private PlayerPiece playerPiece;
 
     /**
-     * @param username            Unique game.player.Player I
+     * Instantiates a player, giving them a username and a player piece.
+     *
+     * @param username the username used to identify the player.
+     * @param playerPiece the player piece which represents the player on the gameboard.
      */
     public Player(String username, PlayerPiece playerPiece) {
         this.username = username;
@@ -31,14 +37,26 @@ public class Player {
         this.playerPiece = playerPiece;
     }
 
+    /**
+     * Adds a new effect type to known effect types previously applied to the player.
+     * @param e The type of the effect that has been previously applied to the player.
+     */
     public void addPreviouslyAppliedEffect(EffectType e) {
         previousAppliedEffect.add(e);
     }
 
+    /**
+     * Adds a new action tile to known action tiles drawn by the player.
+     * @param actionTile The action tile to be added to the already drawn action tiles.
+     */
     public void addDrawnActionTile(ActionTile actionTile) {
         drawnActionTiles.add(actionTile);
     }
 
+    /**
+     * The username which identifies the player.
+     * @return The player's username.
+     */
     public String getUsername() {
         return username;
     }
@@ -53,17 +71,36 @@ public class Player {
     }
     */
 
+    /**
+     * Returns the player piece which represents the player.
+     * @return The player's player piece
+     */
     public PlayerPiece getPlayerPiece() {
         return playerPiece;
     }
 
+    /**
+     * Returns the set of effect types of effects that have been previously applied to the player.
+     * @return
+     */
     public Set<EffectType> getPreviousAppliedEffect() {
         return previousAppliedEffect;
     }
 
-    public static void main(String[] args) { }
-
+    /**
+     * Returns all action tiles drawn by the player.
+     * @return The list of action tiles drawn by the player.
+     */
     public List<ActionTile> getDrawnActionTiles() {
         return drawnActionTiles;
     }
+
+    /**
+     * The entry point of application, for testing only. todo remove this.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) { }
+
+
 }
