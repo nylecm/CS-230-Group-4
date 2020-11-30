1b638febc488e24102a743c3afdad7f0f1d8cd43
SamCox7500 <coxy75@hotmail.co.uk>
2020-11-30 15:08:54 +0000

    loadGame - made back button functional.

--- src/java_/controller/LoadGameController.java
+++ src/java_/controller/LoadGameController.java
@@ -4,10 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +35,10 @@ public class LoadGameController implements Initializable {
         loadGameSelect.setItems(FXCollections.observableArrayList(fileNames));
     }
 
-    public void onBackButtonClicked(ActionEvent actionEvent) {
+    public void onBackButtonClicked(ActionEvent e) throws IOException {
+        Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Pane mainMenu = (Pane) FXMLLoader.load(getClass().getResource("../../view/layout/mainMenu.fxml"));
+        currentStage.setScene(new Scene(mainMenu));
     }
 
     public void onLoadGameButtonClicked(ActionEvent actionEvent) {
