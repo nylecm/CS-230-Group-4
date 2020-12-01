4005a8939269b7e4827ca64fa00ae154a416737b
nylecm <michalnylec@gmail.com>
2020-12-01 00:34:53 +0000

    Constants added.

--- src/java_/game/tile/EffectFactory.java
+++ src/java_/game/tile/EffectFactory.java
@@ -1,10 +1,10 @@
 package java_.game.tile;
 
 public class EffectFactory {
+    private final static String TILE_NO_EFFECT_MESSAGE =
+            "Tile has an effect that is not supported by the factory!";
+
     public static Effect getEffect(TileType type) {
-        if (type == null) {
-            throw new IllegalArgumentException("");
-        }
         if (ActionTile.ACTION_TILE_TYPES.contains(type)) {
             if (type == TileType.ICE) {
                 return new AreaEffect(EffectType.ICE, 1, 4); //todo update duration, and get rid of magic numbers.
@@ -16,7 +16,7 @@ public class EffectFactory {
                 return new PlayerEffect(EffectType.DOUBLE_MOVE, null); //todo -"-
             }
         }
-        throw new IllegalArgumentException("");
+        throw new IllegalArgumentException(TILE_NO_EFFECT_MESSAGE);
     }
 
     public static void main(String[] args) {
