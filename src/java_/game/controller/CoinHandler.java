6cca47947eb810feb36911031d40fab7d9615b24
SamCox7500 <coxy75@hotmail.co.uk>
2020-12-02 21:37:55 +0000

    player_piece_cost.txt created and populated with starting / free player pieces.

---
+++ src/java_/game/controller/CoinHandler.java
@@ -0,0 +1,25 @@
+package java_.game.controller;
+
+import java_.game.player.Player;
+
+public class CoinHandler {
+
+    public static void updateCoins(Player[] players, int winningPlayerIndex) {
+        int nTurns = GameService.getInstance().getTurnCount();
+        int nPlayers = players.length;
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
