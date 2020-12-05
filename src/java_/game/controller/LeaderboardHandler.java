bbaa74cb0dfb269b991707cbe0e56fe93fd8b764
SamCox7500 <coxy75@hotmail.co.uk>
2020-12-05 21:25:20 +0000

    updateLeaderboard method creates new file line for user if first time playing.

--- src/java_/game/controller/LeaderboardHandler.java
+++ src/java_/game/controller/LeaderboardHandler.java
@@ -20,6 +20,7 @@ public class LeaderboardHandler {
         Reader r = new Reader();
         File[] fileNames = r.readFileNames(USER_STATS_FOLDER_DIRECTORY);
         for (File file: fileNames) {
+            boolean[] hasUserBeenFound = new boolean[players.length];
             if (file.getName().equals(gameboardName)) {
 
                 File gameboardFile = new File(USER_STATS_FOLDER_DIRECTORY + "/" + gameboardName);
@@ -27,17 +28,16 @@ public class LeaderboardHandler {
 
                 ArrayList<String> newFileLines = new ArrayList<>();
 
-                int numPlayersInFile = 0;
                 while (in.hasNextLine()) {
-
-                    numPlayersInFile +=1; //todo
 
                     String[] currentPlayerStats = in.nextLine().split(DELIMITER);
                     for (int i = 0; i < players.length; i++) {
                         if (players[i].getUsername().equals(currentPlayerStats[0]) && i == winningPlayerIndex) { //The current line contains the stats of the winning player of the game
                             currentPlayerStats[1] = String.valueOf(Integer.valueOf(currentPlayerStats[1]) + 1); //todo probably being stupid - it's 2am
+                            hasUserBeenFound[i] = true;
                         } else if (i != winningPlayerIndex && players[i].getUsername().equals(currentPlayerStats[0])){ //The current line stores the stats of someone who was in the game but lost.
                             currentPlayerStats[2] = String.valueOf(Integer.valueOf(currentPlayerStats[2]) + 1); //todo probably being stupid - it's 2am
+                            hasUserBeenFound[i] = true;
                         }
                     }
                     String newFileLine = currentPlayerStats[0] + DELIMITER + currentPlayerStats[1] + DELIMITER + currentPlayerStats[2] + DELIMITER;
@@ -48,15 +48,31 @@ public class LeaderboardHandler {
                 for (String line : newFileLines) { //todo
                     fileWriter.println(line);
                 }
+
+                for (int i = 0; i < hasUserBeenFound.length; i++) {
+                    if (!hasUserBeenFound[i] && i == winningPlayerIndex) {
+                        fileWriter.println(players[i].getUsername() + DELIMITER + "1" + DELIMITER + "0" + DELIMITER);
+                    } else if (!hasUserBeenFound[i] && i != winningPlayerIndex) {
+                        fileWriter.println(players[i].getUsername() + DELIMITER + "0" + DELIMITER + "1" + DELIMITER);
+                    }
+                }
+
                 fileWriter.flush();
                 fileWriter.close();
+
+
+
+
             }
         }
     }
 
+
+
+
     public static void main(String[] args) {
-        Player newPlayer = new Player("samcox", null);
-        Player newPlayer2 = new Player("bob101", null);
+        Player newPlayer = new Player("james", null);
+        Player newPlayer2 = new Player("roy", null);
 
         Player[] playerArray = new Player[2];
         playerArray[0] = newPlayer;
