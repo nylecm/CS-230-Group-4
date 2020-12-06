910a5f628421364af9c481358bf5edce066aa470
SamCox7500 <coxy75@hotmail.co.uk>
2020-12-06 13:56:17 +0000

    Fully documented and commented PurchaseHandler.java.

--- src/java_/game/controller/PurchaseHandler.java
+++ src/java_/game/controller/PurchaseHandler.java
@@ -17,6 +17,14 @@ public class PurchaseHandler {
     private static final String PLAYERPIECE_PRICE_DIRECTORY = "data/player_piece_cost.txt";
 
 
+    /**
+     * Purchases a new player piece for the specified player. Their coin balance is updated to reflect this purchase.
+     * @param username The username of th player making the purchase.
+     * @param newPlayerPiece The new player piece being purchased.
+     * @param userAmountOfCoins The user's current coin balance
+     * @return The updated coin balance of the user after the purchase has been made.
+     * @throws IOException If the player piece the player is trying to buy is already owned by the player.
+     */
     public static int buyPlayerPiece(String username, String newPlayerPiece, int userAmountOfCoins) throws IOException {
         ArrayList<String> users = new ArrayList<>();
 
@@ -24,6 +32,7 @@ public class PurchaseHandler {
         File priceFile = new File(PLAYERPIECE_PRICE_DIRECTORY);
         Scanner in = new Scanner(priceFile);
         in.useDelimiter(DELIMITER);
+        //Loops through file until it finds the player piece player piece the player wants to buy and stores its price.
         while (in.hasNextLine()) {
             in.next(); //todo
             String playerPiece = in.next();
@@ -36,11 +45,13 @@ public class PurchaseHandler {
         }
         in.close();
 
-        int newCoinAmount = userAmountOfCoins - piecePrice;
+        int newCoinAmount = userAmountOfCoins - piecePrice; //New balance after transaction
         File userFile = new File(USER_COINFILE_DIRECTORY);
-
         in = new Scanner(userFile);
         in.useDelimiter(DELIMITER);
+
+
+        //Loops through the user coin file until it finds the user making the purchase and stores the line number
         int indexOfNeededLine = 0;
         while (in.hasNextLine()) {
             String tempUser = in.next();
@@ -54,22 +65,27 @@ public class PurchaseHandler {
         String currentLine = users.get(indexOfNeededLine);
         String[] parts = currentLine.split(DELIMITER);
 
+        //Loops through all owned player pieces of the player making the purchase.
+        //Throws an error if they already own the piece they are trying to purchase.
         for (int i = 5; i < parts.length; i++) {
             if (parts[i].equals(newPlayerPiece)) {
                 throw new IllegalArgumentException("Player piece already owned!");
             }
         }
 
+        //Updates the file line of the player making the purchase to reflect new balance
         String newRowData = parts[0] + DELIMITER + newCoinAmount + DELIMITER +
                 parts[2] + DELIMITER + parts[3] + DELIMITER + (Integer.parseInt(parts[4]) + 1) + DELIMITER;
 
         for (int i = 5; i < parts.length; i++) {
             newRowData = newRowData + parts[i] + DELIMITER;
         }
+        //Adds the new purchased player piece to all owned player pieces.
         newRowData = newRowData + newPlayerPiece + DELIMITER;
         users.remove(indexOfNeededLine);
         users.add(newRowData);
 
+        //Writes the updated line to the file.
         FileWriter writer = new FileWriter(USER_COINFILE_DIRECTORY);
         for (String user : users) {
             writer.write(user + System.lineSeparator());
@@ -79,6 +95,12 @@ public class PurchaseHandler {
         return newCoinAmount;
     }
 
+    /**
+     * Returns all owned player pieces for a specified player.
+     * @param username The username of the player who the player pieces belong to.
+     * @return The ArrayList of all player pieces owned by the user.
+     * @throws FileNotFoundException If the user coin file containing the player pieces cannot be found.
+     */
     public static ArrayList<String> getPlayersPurchasedPlayerPieces(String username) throws FileNotFoundException {
         ArrayList<String> ownedPlayerPieces = new ArrayList<>();
 
@@ -86,6 +108,7 @@ public class PurchaseHandler {
         in.useDelimiter(DELIMITER);
         boolean targetUserFound = false;
 
+        //Loops through file until target user is found and then reads all owned player pieces
         while (in.hasNextLine() && !targetUserFound) {
             String curUsername = in.next();
             System.out.println(curUsername);
@@ -109,6 +132,11 @@ public class PurchaseHandler {
         return ownedPlayerPieces;
     }
 
+    /**
+     * Adds all free player pieces (player pieces with a coin cost of 0) to the player pieces owned by the player.
+     * @param username The username of the player who is buying all the free player pieces.
+     * @throws IOException If the free player piece is already owned by the player.
+     */
     public static void buyAllFreePlayerPieces(String username) throws IOException {
         ArrayList<String> freePlayerPieces = getFreePlayerPiecesNames();
         for (String freePlayerPiece : freePlayerPieces) {
@@ -116,6 +144,11 @@ public class PurchaseHandler {
         }
     }
 
+    /**
+     * Returns the names of all free player pieces
+     * @return The ArrayList containing the names of all free player pieces.
+     * @throws FileNotFoundException
+     */
     private static ArrayList<String> getFreePlayerPiecesNames() throws FileNotFoundException {
         Scanner in = new Scanner(new File(PLAYERPIECE_PRICE_DIRECTORY));
         in.useDelimiter(DELIMITER);
@@ -133,7 +166,14 @@ public class PurchaseHandler {
         return freePlayerPiecesNames;
     }
 
-    public static void addNewPlayer(String username, boolean isFirstPlayer) throws IOException {
+    /**
+     * Adds a new player to the file containing all players and their owned player pieces.
+     * The new player will be given all free playe pieces.
+     * @param username the username of the new player to be added to the file.
+     * @param isFirstPlayer True if the player is the first playr of the game.
+     * @throws IOException If the player already exists.
+     */
+    public static void addNewPlayer(String username, boolean isFirstPlayer) throws IOException { //todo remove boolean parameter?
         SimpleDateFormat yyyyddmm = new SimpleDateFormat(DATE_FORMAT);
         Calendar calendar = Calendar.getInstance();
         String dateToday = yyyyddmm.format(calendar.getTime());
