f29d2719a70f260ee4351a968f01b3b2aa5847f3
SamCox7500 <coxy75@hotmail.co.uk>
2020-12-06 00:48:10 +0000

    BackupNewGame.java non-functional.

--- src/BackupNewGame.java
+++ src/BackupNewGame.java
@@ -19,19 +19,20 @@ public class BackupNewGame extends Application {
 
     public static void main(String[] args) throws IOException {
 
+        /*
         File playerPiece1File = new File("src/view/res/img/player_piece/alien_ufo_1.png");
         PlayerPiece player1pp = new PlayerPiece(playerPiece1File);
         Player player1 = new Player("bob101", player1pp);
 
-        File playerPiece2File = new File("alien_ufo_2.png");
+        File playerPiece2File = new File("src/view/res/img/player_piece/alien_ufo_2.png");
         PlayerPiece player2pp = new PlayerPiece(playerPiece2File);
         Player player2 = new Player("user", player2pp);
 
-        File playerPiece3File = new File("homer.png");
+        File playerPiece3File = new File("src/view/res/img/player_piece/homer.png");
         PlayerPiece player3pp = new PlayerPiece(playerPiece3File);
         Player player3 = new Player("user", player3pp);
 
-        File playerPiece4File = new File("red_temp_pp.png");
+        File playerPiece4File = new File("src/view/res/img/player_piece/red_temp_pp.png");
         PlayerPiece player4pp = new PlayerPiece(playerPiece4File);
         Player player4 = new Player("nylecm", player4pp);
 
@@ -48,9 +49,12 @@ public class BackupNewGame extends Application {
 
         GameService.getInstance().loadNewGame(players, gameboardName);
 
+        /*
         Stage currentStage = null;
         Pane game = (Pane) FXMLLoader.load(BackupNewGame.class.getResource("../../view/layout/game.fxml"));
         currentStage.setScene(new Scene(game));
+         */
+
 
 
 
@@ -65,12 +69,42 @@ public class BackupNewGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        File playerPiece1File = new File("src/view/res/img/player_piece/alien_ufo_1.png");
+        PlayerPiece player1pp = new PlayerPiece(playerPiece1File);
+        Player player1 = new Player("bob101", player1pp);
+
+        File playerPiece2File = new File("src/view/res/img/player_piece/alien_ufo_2.png");
+        PlayerPiece player2pp = new PlayerPiece(playerPiece2File);
+        Player player2 = new Player("user", player2pp);
+
+        File playerPiece3File = new File("src/view/res/img/player_piece/homer.png");
+        PlayerPiece player3pp = new PlayerPiece(playerPiece3File);
+        Player player3 = new Player("user", player3pp);
+
+        File playerPiece4File = new File("src/view/res/img/player_piece/red_temp_pp.png");
+        PlayerPiece player4pp = new PlayerPiece(playerPiece4File);
+        Player player4 = new Player("nylecm", player4pp);
 
 
+        Player[] players = new Player[4];
+        players[0] = player1;
+        players[1] = player2;
+        players[2] = player3;
+        players[3] = player4;
 
-        Pane game = (Pane) FXMLLoader.load(BackupNewGame.class.getResource("../../view/layout/game.fxml"));
+        String gameboardName = "oberon_1.txt";
+
+
+        GameService.getInstance().loadNewGame(players, gameboardName);
+
+        Pane game = (Pane) FXMLLoader.load(getClass().getResource("view/layout/game.fxml"));
+
+
         primaryStage.setScene(new Scene(game));
         primaryStage.show();
+
+
+
 
 
 
