305d24814982eb5fe3302b3a9682b091d4aee8df
SamCox7500 <coxy75@hotmail.co.uk>
2020-12-06 21:51:24 +0000

    Fully documented EffectFactory.java.

--- src/java_/game/tile/EffectFactory.java
+++ src/java_/game/tile/EffectFactory.java
@@ -3,7 +3,7 @@ package java_.game.tile;
 import java_.game.controller.GameService;
 
 /**
- *
+ * Creates effects from tile types.
  *
  * @author nylecm
  */
@@ -12,6 +12,13 @@ public class EffectFactory {
             "Tile has an effect that is not supported by the factory!";
     private final static int THREE_BY_THREE_EFFECT_RADIUS = 1;
 
+    /**
+     * Returns an effect from a tile type.
+     *
+     * @param type The type of the floor tile used to create the effect.
+     * @return The effect created from the tile type.
+     * @throws IllegalArgumentException If the tile type is one that does create effects.
+     */
     public static Effect getEffect(TileType type) throws IllegalArgumentException {
 
         if (ActionTile.ACTION_TILE_TYPES.contains(type)) {
