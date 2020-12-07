38e950d4eb45a66b1aa4c2a5e3da99920eec99d0
nylecm <michalnylec@gmail.com>
2020-12-07 02:27:06 +0000

    main menu main doc

--- src/MainMenuMain.java
+++ src/MainMenuMain.java
@@ -4,14 +4,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * Main Class used to run the game.
+ *
+ * @author nylecm
+ */
 public class MainMenuMain extends Application {
+
+    private static final int WIDTH = 720;
+    private static final int HEIGHT = 576;
+    private static final String MAIN_MENU_FXML = "view/layout/mainMenu.fxml";
+
     /**
-     * The type java.java_.Main.
+     * Starts the game, showing the main menu.
+     *
+     * @param primaryStage not used
      */
     public void start(Stage primaryStage) {
         try {
-            Pane root = (Pane) FXMLLoader.load(getClass().getResource("view/layout/mainMenu.fxml"));
-            Scene scene = new Scene(root, 720, 576);
+            Pane root = (Pane) FXMLLoader.load(getClass().getResource(MAIN_MENU_FXML));
+            Scene scene = new Scene(root, WIDTH, HEIGHT);
 
             primaryStage.setScene(scene);
             primaryStage.show();
